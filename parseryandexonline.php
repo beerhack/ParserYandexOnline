488b76a8f2d520ff4aba1efa790a01624bda61a4
beerhack <0xbeerhack@gmail.com>
2013-01-06 14:56:25 +0200

    Add check for uniqueness

--- parseryandexonline.php
+++ parseryandexonline.php
@@ -13,6 +13,7 @@ if(!empty($_POST['howmany'])): ?>
 $howmany = $_POST['howmany'];
 $seconds = $_POST['sleep'];
 $filename = $_POST['filename'];
+$unic = (isset($_POST['unic']) && $_POST['unic']=='ok');
 function curlFunc($url){ 
 	$ch = curl_init(); 
     curl_setopt($ch, CURLOPT_HEADER, 0); 
@@ -29,7 +30,13 @@ for($i=1; $i<=$howmany; $i++){
 	$url ='http://livequeries-front.corba.yandex.net/queries/?ll1=-1.7356459922329044,-1.9623822499999828&ll2=84.54750547846963,209.32668025&limit=1000';
 	$str = curlFunc($url);
 	if(preg_match_all('/query text="([^"]+)"/i',$str,$match)>0){
-		foreach($match[1] as $key){
+		if($unic){
+			$keyarray = array_unique($match[1]);
+			$howmanyfact += sizeof($keyarray);
+		} else {
+			$keyarray = $match[1];
+		}
+		foreach($keyarray as $key){
 			$key = str_replace('&quot;', '"', $key);
 			$key = trim($key);
 			$f = fopen($filename,'at');
@@ -47,12 +54,18 @@ for($i=1; $i<=$howmany; $i++){
 	}
 	sleep($seconds);
 }
+if($unic){
+	echo "Закончили работу. Собрали все $howmany тысяч кейвордов. Из них $howmanyfact уникальных, которые сохранены в файл $filename.";
+} else {
+	echo "Закончили работу. Собрали все $howmany тысяч кейвордов, которые сохранены в файл $filename. На уникальность не проверялись.";
+}
 ?> <?php else : ?>
 <html>
 <form method="POST" action="">
 Сколько тысяч собрать? <input type="text" name="howmany" value="1"><br>
 Можно задержку поставить в секундах <input type="text" name="sleep" value="0"><br>
 Куда сохранить? <input type="text" name="filename" value="resultp.txt"><br>
+Проверить на уникальность по окончании? <input type="checkbox" name="unic" value="ok"><br>
 <input type="submit" value="START">
 </form>
 </html>
